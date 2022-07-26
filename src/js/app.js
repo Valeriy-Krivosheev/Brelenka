0410d71b63e35f2b046fd80f2bff190e2b026f33
Olga <blacksmith@skillpoint.ru>
2022-07-26 12:24:54 +0300

    page

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,11 @@
 
 import Swiper, { Navigation, Pagination } from 'swiper';
 Swiper.use([Navigation, Pagination]);
-//  Slider настройки
 
 
-new Swiper('.image-slider', {
+//  Slider настройки основной страницы
+const mainSlider = document.querySelector('.image-slider')
+new Swiper(mainSlider, {
 	pagination: {
 		el: '.swiper-pagination',
 		type: 'bullets',
@@ -13,21 +14,26 @@ new Swiper('.image-slider', {
 });
 
 
-// Стрелка пролистывания Header
-const height = document.querySelector('.header').clientHeight;
-const arrow = document.querySelector('.header__arrow')
-arrow.addEventListener('click', () => {
-	window.scrollBy({
-		top: (height),
-		behavior: 'smooth'
-	});
-})
+
+// Slider страницы новости подробно
+const newsSlider = document.querySelector('.news-slider')
+new Swiper(newsSlider, {
+	pagination: {
+		el: '.news-pagination',
+		type: 'bullets',
+		clickable: true
+	},
+	navigation: {
+    nextEl: '.news-button-next',
+    prevEl: '.news-button-prev',
+  },
+});
 
 
+
+// Стрелка пролистывания Header основной страницы
 const link = document.querySelector('.header__arrow')
-
-link.addEventListener('click', function () {
-	console.log('hi');
+link?.addEventListener('click', function () {
 	const href = this.getAttribute('href');
 	const scrollTarget = document.getElementById(href);
 	const elementPosition = scrollTarget.getBoundingClientRect().top;
@@ -38,13 +44,12 @@ link.addEventListener('click', function () {
 })
 
 // Burger
-
 const burger = document.querySelector('.burger-menu')
 const burgerText = document.querySelector('.burger-menu__text')
 const navMenu = document.querySelector('.nav__list')
 
 burger.addEventListener('click', () => {
-	burger.classList.toggle('_active');
-	burgerText.classList.toggle('_active');
-	navMenu.classList.toggle('_active');
+	burger.classList.toggle('active');
+	burgerText.classList.toggle('active');
+	navMenu.classList.toggle('active');
 })
